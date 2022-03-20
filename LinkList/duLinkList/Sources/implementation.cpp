d4f46beb1d94e97218abd61a07cc7edf20334da6
Leoscott <PRCSteven06@gmail.com>
2022-03-20 12:06:31 +0800

    both implementations

--- LinkList/duLinkList/Sources/implementation.cpp
+++ LinkList/duLinkList/Sources/implementation.cpp
@@ -72,3 +72,65 @@ void TraverseList_DuL(DuLinkedList L, void (*visit)(ElemType e)) {
         L=L->next;
     }
 }
+int main() {
+    void Display(ElemType e);
+    void (*dis)(ElemType e)=&Display;
+
+    cout<<"Insert案例演示"<<endl;
+    DuLinkedList forInsert,ptr;
+    InitList_DuL(&forInsert);
+    forInsert->data=0;
+    ptr=forInsert;
+    for(int i(1);i<=10;i++) {
+        DuLinkedList newNode=new DuLNode;
+        newNode->data=i;
+        InsertAfterList_DuL(ptr,newNode);
+        DuLinkedList gapNode=new DuLNode;
+        gapNode->data=-i;
+        InsertBeforeList_DuL(ptr,gapNode);
+        ptr=ptr->next;
+    }
+    TraverseList_DuL(forInsert,dis);
+    cout<<endl;
+    cout<<"用InsertAfter来添加节点的,而负数则是使用InsertBefore来添加的"<<"\n\n";
+
+    DuLinkedList head;
+    cout<<"创建新链表中"<<endl;
+    Status s=InitList_DuL(&head);
+    if(s==SUCCESS) {
+        cout<<"成功."<<endl;
+        head->data=0;
+    }
+    else {
+        cout<<"error, I got you"<<endl;
+        return -1;
+    }
+    int data=1;
+    DuLinkedList current=head;
+    cout<<"依次输入节点数据,请不要超过2^31+1. 输入0结束输入"<<endl;
+    bool once=true;
+    while(data) {
+        cin>>data;
+        if(once) {
+            current->data=data;
+            once=false;
+        }
+        else if(data!=0) {
+            DuLinkedList NewNode=new DuLNode;
+            NewNode->data=data;
+            InsertAfterList_DuL(current,NewNode);
+            current=current->next;
+        }
+    }
+    
+    cout<<"你的链表是:"<<endl;
+    TraverseList_DuL(head,dis);
+    cout<<endl;
+    DestroyList_DuL(&head);
+    cout<<"链表已销毁"<<endl;
+    system("pause");
+    return 0;
+}
+void Display(ElemType e) {
+    cout<<e<<' ';
+}
