f1ce1489aad6f24b13ada38551e82c82b59ac471
Leoscott <PRCSteven06@gmail.com>
2022-03-28 20:35:02 +0800

    bxxl

--- Week2/SmallGroup/crud.java
+++ Week2/SmallGroup/crud.java
@@ -4,6 +4,8 @@ public class crud {
     public static void main(String[] args) throws SQLException,ClassNotFoundException{
         Utils_crud crud=new Utils_crud();
         crud.login("root","123456");
-        crud.create("create");
+        //crud.create("create");
+        Book book=new Book("test book", 0, true);
+        crud.insertion(book);
     }
 }
